1eb60b366a39a74f9d0575d2c5bb566122fa7638
skullKim <space1878@naver.com>
2021-06-28 14:40:29 +0900

    input 여러개 다루기

--- eventHandling/even_handling/src/ClassComponentEnvet.js
+++ eventHandling/even_handling/src/ClassComponentEnvet.js
@@ -2,54 +2,42 @@ import React, {Component} from 'react';
 
 class ClassComponentEnvet extends Component {
     state = {
+        username: '',
         message: '',
     }
 
-    constructor(props) {
-        super(props);
-        // this.handleChange = this.handleChange.bind(this);
-        // this.handleClick = this.handleClick.bind(this);
-    }
-
-    // handleChange(e) {
-    //     this.setState({
-    //         message: e.target.value,
-    //     });
-    // }
-    //
-    // handleClick(e) {
-    //     alert(this.state.message);
-    //     this.setState({
-    //         message: '',
-    //     });
-    // }
-
-    handleChnage = (e) => {
+    handleChange = (e) => {
         this.setState({
-            message: e.target.value,
+            [e.target.name]: e.target.value
         });
     }
 
     handleClick = (e) => {
-        alert(this.state.message);
+        alert(`${this.state.username}: ${this.state.message}`);
         this.setState({
+            username: '',
             message: '',
         });
     }
 
-    render() {
+    render(){
         return (
             <div>
                 <h1>practice event</h1>
+                <input
+                    type="text"
+                    name="username"
+                    placeholder="user name"
+                    value={this.state.username}
+                    onChange={this.handleChange}
+                />
                 <input type="text"
                        name="message"
                        placeholder="input anything"
                        value={this.state.message}
-                       onChange={this.handleChnage}
+                       onChange={this.handleChange}
                 />
-                <button
-                    onClick={this.handleClick}
-                >확인</button>
+                <button onClick={this.handleClick}>확인</button>
             </div>
         );
     }
