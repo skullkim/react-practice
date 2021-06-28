d2a47699e2e341f748faf055fd6f1a63d1c3e6dc
skullKim <space1878@naver.com>
2021-06-28 22:32:34 +0900

    컴포넌트에 ref달기 예제

--- ref/ref/src/App.js
+++ ref/ref/src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import ValidationSample from "./validation-sample";
 import CreateRef from "./create-ref";
+import ScrollBox from "./ScrollBox";
 
-function App() {
-  return (
-      <ValidationSample/>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <ScrollBox ref={(ref) => this.scrollBox=ref}/>
+                <button onClick={() => this.scrollBox.scrollToBottom()}>
+                    to bottom
+                </button>
+            </div>
+        );
+    }
 }
 
 export default App;
