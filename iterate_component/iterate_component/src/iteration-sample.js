7d7a6a596280c40a8c19b59cd5b5d175741a2725
skullKim <space1878@naver.com>
2021-06-28 23:18:48 +0900

    데이터 추가 기능

--- iterate_component/iterate_component/src/iteration-sample.js
+++ iterate_component/iterate_component/src/iteration-sample.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const IterationSample = () => {
-    const names = ['1', '2', '3'];
-    const name_list = names.map((name, idx) => <li key={idx}>{name}</li>)
-    return <ul>{name_list}</ul>
+    const [names, setNames] = useState([
+        {id: 1, text: 'a'},
+        {id: 2, text: 'b'},
+        {id: 3, text: 'c'},
+        {id: 4, text: 'd'},
+    ]);
+    const [input_text, setInputText] = useState('');
+    const [next_id, setNextId] = useState(5);
+    const onChange = (e) => setInputText(e.target.value);
+    const onClick = () => {
+        const next_name = names.concat({
+            id: next_id,
+            text: input_text,
+        });
+        setNextId(next_id + 1);//next_id값 1 증가
+        setNames(next_name);//name 값을 업데이트 한다
+        setInputText('');//text를 비운다
+    }
+    const name_list = names.map(name => <li key={name.id}>{name.text}</li>);
+    return(
+        <div>
+            <input value={input_text} onChange={onChange}/>
+            <button onClick={onClick}>add</button>
+            <ul>{name_list}</ul>
+        </div>
+    );
 };
 
 export default IterationSample;
