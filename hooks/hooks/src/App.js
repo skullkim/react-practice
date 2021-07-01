093041be38bfb3a8976455fdf95d7c2ef97658aa
skullKim <space1878@naver.com>
2021-07-01 20:32:20 +0900

    useMemo

--- hooks/hooks/src/App.js
+++ hooks/hooks/src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import React, {useState} from 'react';
 import './App.css';
 // import Counter from "./Counter";
-import Info from "./Info";
+// import Info from "./Info";
+import Average from "./Average";
 
 function App() {
     return (
         <div>
-            <Info/>
+            <Average/>
         </div>
     )
 }
