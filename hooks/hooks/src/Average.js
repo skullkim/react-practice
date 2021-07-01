cf72300bb2f5f3680255875f859898a2e40fbfd2
skullKim <space1878@naver.com>
2021-07-01 21:10:44 +0900

    useRef, useCallback

--- hooks/hooks/src/Average.js
+++ hooks/hooks/src/Average.js
@@ -1,4 +1,4 @@
-import React, {useState, useMemo} from 'react';
+import React, {useState, useMemo, useCallback, useRef} from 'react';
 
 const getAverage = (numbers) => {
     console.log('calculating average');
@@ -11,20 +11,22 @@ const Average = () => {
     const [list, setList] = useState([]);
     const [number, setNumber] = useState('');
     const avg = useMemo(() => getAverage(list), [list]);
+    const inputE1 = useRef(null);
 
-    const onChange = (e) => {
+    const onChange = useCallback((e) => {
         setNumber(e.target.value);
-    };
+    }, []);//컴포넌트가 처음 렌더링 될때만 함수 생성
 
-    const onInsert = (e) => {
+    const onInsert = useCallback((e) => {
         const nextList = list.concat(parseInt(number));
         setList(nextList);
         setNumber('');
-    };
+        inputE1.current.focus();
+    }, [number, list]);
 
     return (
         <div>
-            <input type="text" value={number} onChange={onChange}/>
+            <input type="text" value={number} onChange={onChange} ref={inputE1}/>
             <button onClick={onInsert}>confirm</button>
             <ul>
                 {list.map((value, index) => (
