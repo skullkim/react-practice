728afc5cedb7fe56e79fc19a824e1d7314a65a19
skullKim <space1878@naver.com>
2021-07-06 17:06:11 +0900

    component optimization with useReducer

--- toDoList/to_do_list/src/App.js
+++ toDoList/to_do_list/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef, useCallback, useReducer } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -15,12 +15,26 @@ function createBulkTodos() {
     return array;
 }
 
-const App = () => {
-  const [todos, setTodos] = useState(createBulkTodos);
+function todoReducer(todos, action) {
+    switch (action.type) {
+        case 'INSERT':
+            return todos.concat(action.todo);
+        case 'REMOVE':
+            return todos.filter(todo => todo.id !== action.id);
+        case 'TOGGLE':
+            return todos.map(todo =>
+                todo.id === action.id ? {...todo, checked: !todo.checked} : todo,
+            );
+        default:
+            return todos;
+    }
+}
 
+const App = () => {
+    const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
   // 고유 값으로 사용 될 id
   // ref 를 사용하여 변수 담기
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
 
   const onInsert = useCallback(
       text => {
@@ -29,7 +43,7 @@ const App = () => {
           text,
           checked: false,
         };
-        setTodos(todos => todos.concat(todo));
+        dispatch({type: 'INSERT', todo});
         nextId.current += 1; // nextId 1 씩 더하기
       },
       [],
@@ -37,18 +51,14 @@ const App = () => {
 
   const onRemove = useCallback(
       id => {
-        setTodos(todos => todos.filter(todo => todo.id !== id));
+          dispatch({type: 'REMOVE', id});
       },
       [],
   );
 
   const onToggle = useCallback(
       id => {
-        setTodos(todos =>
-            todos.map(todo =>
-                todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-            ),
-        );
+        dispatch({type: 'TOGGLE', id});
       },
       [],
   );
