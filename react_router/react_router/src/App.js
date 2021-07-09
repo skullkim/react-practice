0d534030fe57914c48e0c2760bd20f33f07e3d6f
skullKim <space1878@naver.com>
2021-07-09 15:25:16 +0900

    nested Router

--- react_router/react_router/src/App.js
+++ react_router/react_router/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {Route, Link} from 'react-router-dom';
 import Home from './Home';
 import About from './About'
-import Profile from './Profile';
+import Profiles from './Profiles';
 
 function App() {
   return (
@@ -11,15 +11,12 @@ function App() {
           <ul>
               <li><Link to={"/"}>home</Link></li>
               <li><Link to={"/about"}>about</Link></li>
-              <li><Link to={"/info"}>info</Link></li>
-              <li><Link to={"/profile/velopert"}>velopert profile</Link></li>
-              <li><Link to={"/profile/iskull"}>iskull profile</Link></li>
-              <li><Link to={"/profile/1"}> profile</Link></li>
+              <li><Link to={"/profiles"}>profile</Link></li>
           </ul>
           <div>
               <Route path={"/"} component={Home} exact={true}/>
               <Route path={["/about", "/info"]} component={About}/>
-              <Route path={"/profile/:username"} component={Profile}/>
+              <Route path={"/profiles"} component={Profiles}/>
           </div>
       </div>
   );
---
+++ react_router/react_router/src/Profiles.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Link, Route} from 'react-router-dom';
+import Profile from './Profile';
+
+const Profiles = () => {
+    return (
+        <div>
+            <h3>User list:</h3>
+            <ul>
+                <li><Link to={"/profiles/velopert"}>velopert</Link></li>
+                <li><Link to={"/profiles/iskull"}>iskull</Link></li>
+            </ul>
+            {/*props설정시 값을 생략하면 자동을 true가 할당된다.*/}
+            {/*Route에 render props를 넣어 컴포넌트 자체를 전달하는 것이 아닌*/}
+            {/*보여주고 싶은 JSX를 넣어줄 수 있다.*/}
+            {/*컴포넌트를 따로 만들기 애매하거나 컴포넌트에 props를 별도로 넣고 싶을때 사용한다.*/}
+            <Route
+                path={"/profiles"}
+                exact
+                render={() => <div>Select user</div>}
+            />
+            <Route path={"/profiles/:username"} component={Profile}/>
+        </div>
+    )
+}
+
+export default Profiles;
