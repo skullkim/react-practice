06b62e2f20157ae6e30426e8994027575f43bfd8
skullKim <space1878@naver.com>
2021-07-28 17:02:34 +0900

    redux-actions

--- redux_react/redux_react/src/modules/counter.js
+++ redux_react/redux_react/src/modules/counter.js
@@ -1,31 +1,23 @@
+import {createAction, handleActions} from 'redux-actions';
 //액션 타입 정의
 //액션 타입은 '모듈 이름/액션 이름' 형식으로 해서 중복을 피한다
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
 //액션 생성 함
-export const increase = () => ({type: INCREASE});
-export const decrease = () => ({type: DECREASE});
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 //초기 상태
 const initialState = {
-    number: 0
+    number: 0,
 };
 
 //reducer 함수
-function counter (state = initialState, action) {
-    switch (action.type) {
-        case INCREASE:
-            return {
-                number: state.number + 1,
-            }
-        case DECREASE:
-            return {
-                number: state.number - 1,
-            }
-        default:
-            return state;
-    }
-}
+//첫 파라미터는 각 액션에 대한 업데이트 함수, 두 번째는 초기 상태
+const counter = handleActions({
+    [INCREASE]: (state, action) => ({number: state.number + 1}),
+    [DECREASE]: (state, action) => ({number: state.number - 1}),
+}, initialState);
 
 export default counter;
--- redux_react/redux_react/src/modules/todos.js
+++ redux_react/redux_react/src/modules/todos.js
@@ -1,3 +1,4 @@
+import {createAction, handleActions} from 'redux-actions';
 //액션 타입 지정
 const CHANGE_INPUT = 'todos/CHNAGE_INPUT';
 const INSERT = 'todos/INSERT';
@@ -5,30 +6,20 @@ const TOGGLE = 'todos/TOGGLE';
 const REMOVE = 'todos/REMOVE';
 
 //액션 생성 함수
-export const changeInput = (input) => ({
-   type: CHANGE_INPUT,
-   input
-});
+//createAction으로 액션을 만들면 액션에 필요한 추가 데이터는 payload라는 이름을 사용한다.
+//{type: CHANGE_INPUT, payload: input}
+export const changeInput = createAction(CHANGE_INPUT, input => input);
 
 let id = 3;//insert 한번 호출 시 +1
-export const insert = (text) => ({
-   type: INSERT,
-   todo: {
-      id: id++,
-      text,
-      done: false
-   }
-});
+export const insert = createAction(INSERT, text => ({
+   id: id++,
+   text,
+   done: false,
+}));
 
-export const toggle = (id) => ({
-   type: TOGGLE,
-   id
-});
+export const toggle = createAction(TOGGLE, id => id);
 
-export const remove = (id) => ({
-   type: REMOVE,
-   id
-});
+export const remove = createAction(REMOVE, id => id);
 
 const initialState = {
    input: '',
@@ -46,33 +37,24 @@ const initialState = {
    ],
 };
 
-function todos(state = initialState, action) {
-   switch(action.type) {
-      case CHANGE_INPUT:
-         return {
-            ...state,
-            input: action.input
-         };
-      case INSERT:
-         return {
-            ...state,
-            todos: state.todos.concat(action.todo)
-         };
-      case TOGGLE:
-         return {
-            ...state,
-            todos: state.todos.map(todo =>
-                todo.id === action.id ? {...todo, done: !todo.done} : todo
-            )
-         };
-      case REMOVE:
-         return {
-            ...state,
-            todos: state.todos.filter(todo => todo.id !== action.id)
-         };
-      default:
-         return state;
-   }
-}
+//createAction을 사용했기 때문에 모든 값은 action.payload로 조회 한다.
+const todos = handleActions({
+   [CHANGE_INPUT]: (state, action) => ({...state, input: action.payload}),
+   [INSERT]: (state, action) => ({
+      ...state,
+      todos: state.todos.concat(action.payload),
+   }),
+   //이와 같이 payload를 사용하는 대신 비구조화 할당을 통해 payload이름을 새로 설정할 수 있다.
+   [TOGGLE]: (state, {payload: id}) => ({
+      ...state,
+      todos: state.todos.map(todo =>
+          todo.id === id ? {...todo, done: !todo.done} : todo,
+      ),
+   }),
+   [REMOVE]: (state, action) => ({
+      ...state,
+      todos: state.todos.filter(todo => todo.id !== action.payload),
+   }),
+}, initialState);
 
 export default todos;
